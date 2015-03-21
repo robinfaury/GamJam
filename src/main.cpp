a3594a46808b816155edaf0aa399f0ca346a4aea
Deliss <morganebris@gmail.com>
2015-03-21 18:07:54 +0100

    sd

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char* argv[])
 {
 	Parser p = Parser();
 	p.loadLevel("../GamJam/res/sauvegarde.txt");
+	
+	p.saveLevel("../GamJam/res/sauvegarde2.txt");
 	std::cin.get();
 
 	return 0;
