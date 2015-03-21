a81a1cc59afabe6ff0aa88181d91e92ac244e90c
jventalon <justine.ventalon@utbm.fr>
2015-03-21 22:59:30 +0100

    main clean

--- src/main.cpp
+++ src/main.cpp
@@ -18,19 +18,5 @@ int main(int argc, char* argv[])
 		boobs.init();
 		boobs.run();
 
-
-	/*Player p = Player();
-	p.setDirection(glm::vec2(1, 0));
-	p.setVelocity(glm::vec2(5, 0));
-	p.setAcceleration(glm::vec2(2, 0));
-	p.setPosition(glm::vec2(0, 0));
-	std::cout << "position :" <<p.getPosition().x << "," << p.getPosition().y << std::endl;
-	p.move(glm::vec2(0, 0), 10);
-	std::cout << "position :" << p.getPosition().x << "," << p.getPosition().y << std::endl;
-	p.move(glm::vec2(0, 0), 10);
-	std::cout << "position :" << p.getPosition().x << "," << p.getPosition().y << std::endl;
-	p.move(glm::vec2(0, 0), 10);
-	std::cout << "position :" << p.getPosition().x << "," << p.getPosition().y << std::endl;*/
-
 	return 0;
 } 
